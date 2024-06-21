e410f8205ae71c3c430291588844bf6774ede18c
Cem Jecer Marquez <cemjecermarquez015@outlook.com>
2024-06-21 19:46:38 +0800

    fixed bug where empty complaints list then can't create new complaints, one existing bug to work and break down the complaints component to several components, still missing login and register page

--- react-app-project/src/modules/complaints/page/Complaints.jsx
+++ react-app-project/src/modules/complaints/page/Complaints.jsx
@@ -9,6 +9,9 @@ export function Complaints() {
     const dialogEditRef = useRef(null);
     const dialogAddRef = useRef(null);
     const inputComplaintRef = useRef(null);
+    // const statusRef = useRef(null);
+    // const officerRef = useRef(null);
+
     const [complaintId, setComplaintId] = useState(null);
     const [isEmpty, setIsEmpty] = useState(false);
     const [complaints, setComplaints] = useState([
@@ -38,32 +41,47 @@ export function Complaints() {
         },
     ])
 
+    // dummy insert one new complaint
     const [complaintInput, setComplaintInput] = useState({
-        key: complaints[complaints.length - 1].key + 1,
+        key: complaints.length !== 0 ? complaints[complaints.length - 1].key + 1 : 0,
         officer: "John Doe",
         complaint_no: "CP-202433",
         date: new Date("February 29, 2024"),
         description: "",
         status: "PENDING"
     })
-    
+    // state for staging complaints update
     const [complaintUpdate, dispatch] = useReducer(updateComplaints, complaints);
 
-
+    // function to handle viewing a complaint
     function handleView(selectedComplaint) {
         dialogEditRef.current.showModal();
         setComplaintId(selectedComplaint);
     }
 
+    // function getMap() {
+    //     if(!statusRef.current) {
+    //         statusRef.current = new Map();
+    //     }
+    //     if(!officerRef.current) {
+    //         officerRef.current = new Map();
+    //     }
+
+    //     return [statusRef.current, officerRef.current];
+    // }
+
+    //function to close modal
     function handleClose() {
         dialogEditRef.current.close();
         dialogAddRef.current.close();
         inputComplaintRef.current.value = "";
     }
     
+    // reducer function that updates the staging state for initial complaints before updating and rendering
     function updateComplaints(currentState,action) {
         switch(action.type) {
             case "status": {
+            
                 return currentState.map((complaint) => {
                     return (complaintId === complaint.key ? {...complaint, status: action.value.toUpperCase()} : {...complaint});
                 })
@@ -81,17 +99,25 @@ export function Complaints() {
             case "add": {
                 return [...currentState, action.value];
             }
+            case "add-from-empty": {
+                return [action.value];
+            }
         }
     }
 
+    // proceed staged updates to a state that renders the final complaint data
     function handleUpdate() {
         setComplaints(complaintUpdate);
         dialogEditRef.current.close();
+        // const mapStatus = getMap()[0];
+        // const nodeStatus = mapStatus.get(statusKey);
+        // nodeStatus.value
         inputComplaintRef.current.value = "";
     }
 
+    // function to handle delete complaint
     function handleDelete(id) {
-        if(complaints.length !== 1) {
+        if(complaints.length !== 0) {
             setComplaints(complaints.filter((complaint) => {
                 return complaint.key !== id;
             }))
@@ -102,41 +128,28 @@ export function Complaints() {
         
     }
 
+    // function to handle show modal for input of a single complaint
     function handleAddComplaint() {
         dialogAddRef.current.showModal();
     }
 
+    // function to submit the input complaint
     function handleSubmitComplaint() {
-        setComplaints([...complaints,{...complaintInput, key: complaints[complaints.length - 1].key + 1}]);
-        dispatch({type: "add", value: {...complaintInput, key: complaints[complaints.length - 1].key + 1}});
+        if(complaints.length > 0) {
+            setComplaints([...complaints,{...complaintInput, key: complaints[complaints.length - 1].key + 1}]);
+            dispatch({type: "add", value: {...complaintInput, key: complaints[complaints.length - 1].key + 1}});
+        }else {
+            setComplaints([{...complaintInput, key: 0}]);
+            dispatch({type: "add-from-empty", value: {...complaintInput, key: 0}});
+        }
         handleClose();
     }
 
+    //used to modify the description of a complaint
     function handleComplaintInput(description) {
         setComplaintInput((prevState) => {return {...prevState, description: description}});
     }    
-    function updateComplaints(currentState,action) {
-        switch(action.type) {
-            case "status": {
-                return currentState.map((complaint) => {
-                    return (complaintId === complaint.key ? {...complaint, status: action.value.toUpperCase()} : {...complaint});
-                })
-            }
-            case "officer": {
-                return currentState.map((complaint) => {
-                    return (complaintId === complaint.key ? {...complaint, officer: action.value} : {...complaint});
-                })
-            }
-            case "delete": {
-                return currentState.filter((complaint) => {
-                    return complaint.key !== action.id;
-                })
-            }
-            case "add": {
-                return [...currentState, action.value];
-            }
-        }
-    }
+
     return (
         <div className="complaints">
             <div className="complaints-main">
@@ -187,7 +200,7 @@ export function Complaints() {
                                             Update Officer
                                         </label>
                                         <select onChange={(e) => {dispatch({type: "officer", value: e.target.value})}} id="select-officer">
-                                            <option defaultValue={"Bea N."}>Bea N.</option>
+                                            <option>Bea N.</option>
                                             <option>Osten D.</option>
                                             <option>Bruno M.</option>
                                         </select>
@@ -195,7 +208,7 @@ export function Complaints() {
                                             Update Status
                                         </label>
                                         <select onChange={(e) => {dispatch({type: "status", value: e.target.value})}} id="select-status">
-                                            <option defaultValue={"pending"}>Pending</option>
+                                            <option value="pending">Pending</option>
                                             <option value="complete">Complete</option>
                                         </select>
                                     </Fragment>
